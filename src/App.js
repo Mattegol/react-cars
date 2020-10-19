27d21037a52afda45ec5a0006dc5736a720250e2
Mats Egnell <MEgnell@NORDIC.HRUK.PRI>
2020-10-19 18:33:03 +0200

    Add car list json and table to match in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    carList: [
+      { brand: "Saab", model: "900 Turbo", year: "1992" },
+      { brand: "Volvo", model: "745 GL", year: "1988" },
+      { brand: "BMW", model: "525i", year: "2001" },
+      { brand: "Nissan", model: "Leaf", year: "2012" },
+      { brand: "Fiat", model: "Uno", year: "1985" }
+    ]
+  }
+
+  render() {
+
+    const rows = this.state.carList.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.brand}</td>
+          <td>{row.model}</td>
+          <td>{row.year}</td>
+          <td>
+            <Button className="btn btn-primary">View info</Button>
+          </td>
+        </tr>
+      )
+    })
+
+    return (
+      <div className="App">
+        <Container>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Year</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {rows}
+            </tbody>
+          </Table>
+        </Container>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
